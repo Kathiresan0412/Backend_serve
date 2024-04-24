57ff371f721559645ad13c53ee34c405ff51c511
Jathusan <106618528+Kathiresan0412@users.noreply.github.com>
2024-04-24 22:12:50 +0530

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $hidden = [
+    public $hidden = [
         'password',
         'remember_token',
     ];
