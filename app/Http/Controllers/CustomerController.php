3255c3ca6863442ed56d6ff648f33569d5c34574
Kathiresan0412 <thuraijathusan@gmail.com>
2024-04-25 10:21:46 +0530

    ss

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -14,34 +14,41 @@ class CustomerController extends Controller
     public function index()
     {
         $customers = DB::table('customers')
-        ->select('customers.id as id', 'customers.user_id', 'users.name as name',
-         'users.email','users.mobile','users.img','users.user_name','users.role','users.password')
-        ->leftJoin('users', 'users.id', '=', 'customers.user_id')->get();
+            ->select(
+                'customers.id as id',
+                'customers.user_id',
+                'users.name as name',
+                'users.email',
+                'users.mobile',
+                'users.img',
+                'users.user_name',
+                'users.role',
+                'users.password'
+            )
+            ->leftJoin('users', 'users.id', '=', 'customers.user_id')->get();
         return response()->json($customers);
     }
     // Store a new customer
     public function store(Request $request)
     {
         // Create a new user
-    $user = new User();
-    $user->user_name = $request->user_name;
-    $user->email = $request->email;
-    $user->name = $request->name;
-    $user->role = $request->role;
-    $user->password = $request->password;
-    $user->mobile = $request->mobile;
-    $user->img = $request->img;
-    $user->save();
+        $user = new User();
+        $user->user_name = $request->user_name;
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->role = $request->role;
+        $user->password = $request->password;
+        $user->mobile = $request->mobile;
+        $user->img = $request->img;
+        $user->save();
 
-    // Create a new customer associated with the user
-    $customer = new Customer();
-    $customer->user_id = $user->id;
-    $customer->save();
+        // Create a new customer associated with the user
+        $customer = new Customer();
+        $customer->user_id = $user->id;
+        $customer->save();
 
-    // Return a response indicating successful creation
-    return response()->json(['message' => 'Customer created successfully.', 'customer' => $customer]);
-
-
+        // Return a response indicating successful creation
+        return response()->json(['message' => 'Customer created successfully.', 'customer' => $customer]);
     }
 
     // Get a customer by ID
@@ -51,32 +58,47 @@ class CustomerController extends Controller
         // $customer->user;
         // return response()->json($customer);
         $customers = DB::table('customers')
-        ->select('customers.id as id', 'customers.user_id', 'users.name as name',
-         'users.email','users.mobile','users.img','users.user_name','users.role','users.password')
-        ->leftJoin('users', 'users.id', '=', 'customers.user_id')
-        ->where('customers.id',$id)  ->first();
+            ->select(
+                'customers.id as id',
+                'customers.user_id',
+                'users.name as name',
+                'users.email',
+                'users.mobile',
+                'users.img',
+                'users.user_name',
+                'users.role',
+                'users.password'
+            )
+            ->leftJoin('users', 'users.id', '=', 'customers.user_id')
+            ->where('customers.id', $id)->first();
         return response()->json($customers);
     }
 
     // Update a customer
     public function update(Request $request, $id)
     {
-    $user = User::findOrFail($id);
-    $user->user_name = $request->user_name;
-    $user->email = $request->email;
-    $user->name = $request->name;
-    $user->role = $request->role;
-    $user->password = $request->password;
-    $user->mobile = $request->mobile;
-    $user->img = $request->img;
-    $user->save();
-    $customer = Customer::where('user_id', $user->id)->first();
-    if ($customer) {
-        $customer->save();
-    }
-    $customer->user();
-    return response()->json($customer);
 
+
+        $customer = Customer::findOrFail($id);
+        if ($customer) {
+            $user_id = $customer->user_id;
+            $user = User::findOrFail($user_id);
+            if ($user) {
+                $user->user_name = $request->user_name;
+                $user->email = $request->email;
+                $user->name = $request->name;
+                $user->role = $request->role;
+                $user->password = $request->password;
+                $user->mobile = $request->mobile;
+                $user->img = $request->img;
+                $user->save();
+            }
+            $customer->save();
+        }
+
+        $customer = Customer::where('user_id', $user->id)->first();
+        $customer->user();
+        return response()->json($customer);
     }
 
     // Delete a customer
